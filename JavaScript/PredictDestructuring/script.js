36c98cb109f76d10b6cae1560524b6b4ae0e3280
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-11-20 16:14:59 -0500

    added guesses for the assignment

---
+++ JavaScript/PredictDestructuring/script.js
@@ -0,0 +1,21 @@
+//Problem 1
+console.log(randomCar)//output Tesla
+console.log(otherRandomCar)//output Mercedes
+
+//Problem 2
+console.log(name)//output Elon
+console.log(otherName)//output Elon
+
+//Problem 3
+console.log(password)//output 12345
+console.log(hashedPassword)//output undefined
+
+//Problem 4
+console.log(first == second)//output false
+console.log(first == third)//output true
+
+//Problem 5
+console.log(key)//output value
+console.log(secondKey)//output error secondKey is already named
+console.log(secondKey[0])//output 1
+console.log(willThisWork)//output 5
