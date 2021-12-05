2365738d1e591b7e452201234ba6d38ba5d30f3c
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-12-05 09:09:48 -0500

    got rid of consol.log

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,6 +1,5 @@
 const PersonCard = (props) => {
     const {firstName, lastName, age, hairColor} = props.person
-    console.log("props", props);
     return (
         <>
             <h1>{lastName}, {firstName}</h1>
