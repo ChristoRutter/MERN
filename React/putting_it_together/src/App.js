acc58b43ca962f381ad8712ed0b492d3e0d83f50
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-12-07 18:47:07 -0500

    Putting it Together Project

---
+++ React/putting_it_together/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from "./components/PersonCard";
+
+function App() {
+  const person = [{
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 45,
+    hairColor: 'Black'
+  },
+  {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 88,
+    hairColor: 'Brown'
+  },
+  {
+    firstName: 'Millard',
+    lastName: 'Fillmore',
+    age: 50,
+    hairColor: 'Brown'
+  },
+  {
+    firstName: 'Maria',
+    lastName: 'Smith',
+    age: 62,
+    hairColor: 'Brown'
+  }]
+
+  return (
+    <div className="App">
+      <PersonCard person = {person[0]}/>
+      <PersonCard person = {person[1]}/>
+      <PersonCard person = {person[2]}/>
+      <PersonCard person = {person[3]}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/putting_it_together/src/components/PersonCard.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const PersonCard = (props) => {
+    const {firstName, lastName, age, hairColor} = props.person
+    const [addAge, setAddAge] = useState(age)
+
+    const increase = () => {
+        setAddAge(addAge + 1)
+    }
+    return (
+        <>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {addAge}</p>
+            <p>Hair Color: {hairColor}</p>
+            <button onClick={increase}>Increase Age</button>
+        </>
+    )
+}
+export default PersonCard;
