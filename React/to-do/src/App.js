0d197690e9a78ebb73a13a129c2220cda45b19c4
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-12-17 06:10:24 -0500

    update

--- React/to-do/src/App.js
+++ React/to-do/src/App.js
@@ -1,19 +1,16 @@
 import React, {useState} from 'react';
 import Form from './components/Form'
-import Display from './components/Display'
 import './App.css';
 
 function App() {
 
   const [list, setList] = useState([])
-  const[checker, setChecker] = useState(false)
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
       To Do:
     </h1>
-    <Form list={list} setList={setList} checker={checker}/>
-    <Display list={list} setList={setList} checker={checker} setChecker={setChecker}/>
+    <Form list={list} setList={setList}/>
     </div>
   );
 }
--- React/to-do/src/components/Form.js
+++ React/to-do/src/components/Form.js
@@ -5,12 +5,16 @@ const Form = (props) => {
     const {list, setList, checker} = props
     const [item, setItem] = useState("")
 
+    const deleteById = (idFromBelow)=>{
+        setList(list.filter((current, index)=>current.id !== idFromBelow))
+    }
+
+    
     const addStuff = (e) => {
         e.preventDefault()
         setList([...list, {
             item: item,
             id: Math.floor(Math.random() * 100000000),
-            checker: checker
         }])
         
         setItem("")
@@ -18,15 +22,53 @@ const Form = (props) => {
     }
 
     return (
+        <div>
+            <form onSubmit = {addStuff}>
+                <p>
+                    <input type="text" onChange = {(e)=>setItem(e.target.value)} value={item}
+                    className="border-2 border-black m-5 shadow-lg"></input>
+                </p>
+                <input type="submit" value="Add" 
+                className="bg-blue-500 p-5 rounded-md text-white"></input>
+            </form>
 
-        <form onSubmit = {addStuff}>
-            <p>
-                <input type="text" onChange = {(e)=>setItem(e.target.value)} value={item}
-                className="border-2 border-black m-5 shadow-lg"></input>
-            </p>
-            <input type="submit" value="Add" 
-            className="bg-blue-500 p-5 rounded-md text-white"></input>
-        </form>
+            <div>
+            <h2 className="m-5 text-2xl underline">
+                Items:
+            </h2>
+            <div>
+
+                {list.map((current, index) => (
+                    <div 
+                    key={index}>
+                        <p>
+                            <span
+                            {...checker === true?
+                                {className: "line-through"}
+                                :null
+
+                            }
+                            
+                            >
+
+                                {current.item}
+                            </span>
+                            <input
+                                className="m-2"
+                                type="checkbox"
+                            
+                            ></input>
+                            <button 
+                                className="bg-black p-1 m-3 rounded-md text-white"
+                                onClick={()=>deleteById(current.id)}
+                            >Delete </button>
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+        </div>
     )
 }
 
