227fdec349853fc53685c3d46050380dbb29bf27
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-12-23 05:53:57 -0500

    condensed into two components

--- React/routing/src/App.js
+++ React/routing/src/App.js
@@ -2,12 +2,9 @@ import React, {useState} from 'react';
 import {Router, Link} from '@reach/router';
 import './App.css';
 import Home from './components/Home'
-import Four from './components/Four'
 import Color from './components/Color';
-import String from './components/String'
 
 function App() {
-  const [number, setNumber] = useState(0)
   const [text, setText] = useState("")
   const [color, setColor] = useState("")
   const [bcolor ,setBColor] = useState("")
@@ -19,34 +16,20 @@ function App() {
       </div>
 
       <form>
-        <Link to = {`/${number}`}>Number</Link>
-        <p>
-        <label>Enter a number: </label>
-        <input onChange={(e)=> setNumber(e.target.value)} type="number" value = {number} name="number"></input>
-        </p>
+        
 
-        <Link to = {`/String/${text}`}>Word</Link>
+        <Link to = {`/${text}`}>Word</Link>
         <p>
         <label>Enter a word/phrase: </label>
         <input onChange={(e)=> setText(e.target.value)} type="text" value = {text} name="text"></input>
         </p>
 
-        <Link to = {`Color/${text}/${color}/${bcolor}`}>Add color</Link>
-        <p>
-        <label>Change the color of the text: </label>
-        <input onChange={(e)=> setColor(e.target.value)} type="text" value = {color} name="color"></input>
-        </p>
-        <p>
-        <label>Change the background color of the text: </label>
-        <input onChange={(e)=> setBColor(e.target.value)} type="text" value = {bcolor} name="bcolor"></input>
-        </p>
       </form>
 
       <Router>
         <Home path="/Home" />
-        <Four path="/:number" />
-        <String path="String/:string" />
-        <Color path="Color/:string/:color/:bcolor" />
+        <Color path="/:string" />
+        <Color path="/:string/:color/:bcolor" />
         
       </Router>
     </div>
--- React/routing/src/components/Color.js
+++ React/routing/src/components/Color.js
@@ -1,17 +1,26 @@
 import React from 'react'
 
 const Color = (props) => {
-
+    if(isNaN(props.string)){
     return (
-
-
         <h1
-        style={{
+        style={
+            props.color !== undefined?
+            {
             color: props.color,
             backgroundColor: props.bcolor
-        }}
+        }
+        :{}
+    
+    }
         >Your word is: {props.string}</h1>
     )
+    }
+    else{
+        return(
+        <h1>Your number is: {props.string}</h1>
+        )
+    }
 }
 
 export default Color
