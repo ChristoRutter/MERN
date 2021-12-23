0c5857007e098cc4637ea34c172d275182fcf470
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-12-23 05:58:46 -0500

    added color changer to the form

--- React/routing/src/App.js
+++ React/routing/src/App.js
@@ -18,10 +18,18 @@ function App() {
       <form>
         
 
-        <Link to = {`/${text}`}>Word</Link>
+        <Link to = {`/${text}/${color}/${bcolor}`}>Word</Link>
         <p>
         <label>Enter a word/phrase: </label>
         <input onChange={(e)=> setText(e.target.value)} type="text" value = {text} name="text"></input>
+        </p>
+        <p>
+        <label>Enter a text color: </label>
+        <input onChange={(e)=> setColor(e.target.value)} type="text" value = {color} name="text"></input>
+        </p>
+        <p>
+        <label>Enter a background color: </label>
+        <input onChange={(e)=> setBColor(e.target.value)} type="text" value = {bcolor} name="text"></input>
         </p>
 
       </form>
