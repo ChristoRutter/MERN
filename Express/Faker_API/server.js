553b456aa7f51e265a1032ba4772085a1be63bcc
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-12-23 07:56:43 -0500

    faker api more

--- Express/Faker_API/server.js
+++ Express/Faker_API/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 
 
+app.use(express.json)
+app.use(express.urlencoded({extended: true}));
 
 
+
+// const createUser = () => {
+
+// }
+
+app.get("/api/faker", (request, response) => {
+    const name = faker.name.firstName();
+    const email = faker.internet.email();
+    response.json({
+        firstName: name,
+        userEmail: email
+    })
+})
 
 
 
