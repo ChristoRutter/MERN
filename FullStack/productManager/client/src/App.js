7f2e1ba57580f0d427456b1da0b732d1c9946d8c
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2022-01-23 13:19:27 -0500

    edit

--- FullStack/productManager/client/src/App.js
+++ FullStack/productManager/client/src/App.js
@@ -5,7 +5,7 @@ import Form from './components/Form'
 function App() {
   return (
     <div className="App">
-      
+      <h1>Product Manager</h1>
     </div>
   );
 }
--- FullStack/productManager/client/src/components/Form.js
+++ FullStack/productManager/client/src/components/Form.js
@@ -2,6 +2,21 @@ import React from "react";
 
 const Form = (props) => {
 
+    <div>
+        <h1>Product Manager</h1>
+        <p>
+            <label>Title: </label>
+            <input type="text"></input>
+        </p>
+        <p>
+            <label>Price: </label>
+            <input type="number"></input>
+        </p>
+        <p>
+            <label>Description: </label>
+            <input type="text"></input>
+        </p>
+    </div>
 
 
 }
